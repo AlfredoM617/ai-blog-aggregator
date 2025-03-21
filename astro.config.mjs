11939f5403dcc24aef26591a10c9524b0e35e036
Alfredo Morales <alfredo@cambridgeselect.com>
2025-03-21 16:57:26 -0400

    Add Vercel adapter and enable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  output: 'server', // ✅ Required for dynamic routing & SSR
+  adapter: vercel(), // ✅ Required for deploying to Vercel
+  integrations: [mdx(), sitemap()]
 });
